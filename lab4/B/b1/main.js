47725205458b8b45aab569d0d09f35c0a53f036b
Ahmed Saladin <as.yuri74@gmail.com>
2021-04-25 15:49:11 +0200

    finishing rectangle constructor

---
+++ lab4/B/b1/main.js
@@ -0,0 +1,23 @@
+/*
+Make your own simulation for a simple linked list custom Object,
+that accepts objects with a single numeric property value in an ascending order.
+1- Enqueue a valueas long as the value is in the sequenceotherwise through 
+    an exception(push an item at the end of the list with the passed value).
+
+2- Insertan iteminspecific place as long as the value 
+    is missing from the sequenceotherwise through an exception.
+
+3- Pop a value (remove an item from the end of the list).
+
+4- Remove an item from specific placewith the required value,
+    if the value is not added return a message with “data not found”.
+    
+5- Dequeue a value (removeanitem from the beginningof the list).
+
+6- Display thecontentof the list.
+
+7- Ensure that there is no duplication in your entered values. 
+
+8- You can add any property you need.
+
+*/
