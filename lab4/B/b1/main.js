ee5f921c4416e799ed3cb5a11cd8bc98f7f5a087
Ahmed Saladin <as.yuri74@gmail.com>
2021-04-26 14:04:43 +0200

    exc about oop

--- lab4/B/b1/main.js
+++ lab4/B/b1/main.js
@@ -4,7 +4,7 @@ that accepts objects with a single numeric property value in an ascending order.
 1- Enqueue a valueas long as the value is in the sequenceotherwise through 
     an exception(push an item at the end of the list with the passed value).
 
-2- Insertan iteminspecific place as long as the value 
+2- Insertan item in specific place as long as the value 
     is missing from the sequenceotherwise through an exception.
 
 3- Pop a value (remove an item from the end of the list).
@@ -21,3 +21,98 @@ that accepts objects with a single numeric property value in an ascending order.
 8- You can add any property you need.
 
 */
+
+var List = [];
+
+function Node(vlu) {
+    return {
+        next: null,
+        value: vlu,
+        previous: null,
+    }
+}
+
+
+// List.push(element);
+// List.push(ele);
+// List.push(ele);
+
+// Enqueue(e, List);
+
+// function Enqueue(node, arr) {
+//     if (arr.length < 1) {
+//         arr.push(node);
+//     // } else {
+//     //     var counter = 0;
+//     //     var there = 0
+//     //     while (counter < arr.length) {
+//     //         var there = (arr[counter].value == node.value) ? there++ : there;
+//     //         if (arr[counter].value < node.value) {
+//     //             //case 1 arr.next not null
+//     //             if (there <= 1)
+//     //                 arr.push(node);
+//     //             arr[arr.length - 1].previous = counter;
+//     //             arr[arr.length - 1].next = arr[counter].next;
+//     //             arr[counter].next = arr.length - 1;
+//     //             //case 2 arr.next null
+//     //         }
+//     //         // } else {
+//     //         //     var prv = arr[counter];
+//     //         //     node.previous =
+//     //         //         arr[counter] = node;
+//     //         // }
+//     //         counter++;
+//     //     }
+//     }
+// }
+
+function LinkedList() {
+    return {
+        head: null,
+        tail: null,
+        length: 0,
+        enqueue: function () { },
+        dequeue: function () { },
+        insert: function (value) {
+            var new_node = new Node(value);
+            if (this.length < 1) {
+                this.head = new_node;
+                this.tail = new_node;
+            } else {
+                this.tail.next = new_node;
+                new_node.previous = this.tail;
+                this.tail = new_node;
+            }
+            this.length++;
+            return this;
+        },
+        remove: function () { },
+        pop: function () {
+            if (this.length == 1) {
+                var n = head;
+                this.head = null;
+                this.tail = null;
+                return n;
+            }
+            if (this.head == null) {
+                throw "List is empty."
+            }
+            var n = this.tail;
+            this.tail = n.previous;
+            this.tail.next = null;
+            this.length--
+            n.previous = null;
+            return n;
+        },
+        displayInfo: function () { }
+    }
+}
+var list = new LinkedList();
+list.insert(15);
+list.insert(12);
+list.insert(11);
+var x = list.insert(10);
+var r = list.pop();
+console.log(r);
+console.log(x);
+
---
+++ lab4/ref/exc/main.js
@@ -0,0 +1,43 @@
+function Person(nm, ag, tl) {
+    return {
+        name: nm,
+        age: ag,
+        title: tl,
+        toString: function () {
+            return `your name is ${this.name} and your age is ${this.age} and you work as ${this.title}.`
+        }
+    }
+}
+
+var Pperson = function (nm, ag, tl) {
+
+    this.name = nm;
+    this.age = ag;
+    this.title = tl;
+    var born = "1522";
+    this.getBorn = function () {
+        return `you born at ${this.born}`;
+    }
+}
+Pperson.prototype.toString = function () {
+    return `your name is ${this.name} and your age is ${this.age} and you work as ${this.title}.`
+}
+Pperson.prototype.bborn = function () {
+    return `you born at ${this.born}`;
+}
+var p1 = new Person("ahmed", "15", "dev");
+p1.birthdate = "12-5-2021";
+p1.like = "banana";
+console.log(p1);
+
+console.log(p1.toString());
+
+var p2 = new Pperson("aa", "15", "dev");
+p2.birthdate = "12-5-2021";
+p2.like = "banana";
+console.log(p2);
+console.log(p2.toString());
+console.log(p2.bborn());
+console.log(p2.getBorn());
+
+
