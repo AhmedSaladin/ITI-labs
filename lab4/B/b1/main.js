5f448bc99ee87c2615bf61fbb8de5864db28ba25
Ahmed Saladin <as.yuri74@gmail.com>
2021-04-26 19:50:41 +0200

    adding get method

--- lab4/B/b1/main.js
+++ lab4/B/b1/main.js
@@ -1,28 +1,24 @@
 /*
 Make your own simulation for a simple linked list custom Object,
 that accepts objects with a single numeric property value in an ascending order.
-1- Enqueue a valueas long as the value is in the sequenceotherwise through 
-    an exception(push an item at the end of the list with the passed value).
 
 2- Insertan item in specific place as long as the value 
-    is missing from the sequenceotherwise through an exception.
+is missing from the sequence otherwise through an exception.
 
-3- Pop a value (remove an item from the end of the list).
-
-4- Remove an item from specific placewith the required value,
-    if the value is not added return a message with “data not found”.
-    
-5- Dequeue a value (removeanitem from the beginningof the list).
-
-6- Display thecontentof the list.
+4- Remove an item from specific place with the required value,
+if the value is not added return a message with “data not found”.
 
 7- Ensure that there is no duplication in your entered values. 
 
 8- You can add any property you need.
 
+1- Enqueue a value as long as the value is in the sequence otherwise through
+            an exception(push an item at the end of the list with the passed value). 
+    3- Pop a value (remove an item from the end of the list). --done
+    5- Dequeue a value (remove an item from the beginning of the list). --done
+    6- Display the contentof the list. --done
 */
 
-var List = [];
 
 function Node(vlu) {
     return {
@@ -32,48 +28,12 @@ function Node(vlu) {
     }
 }
 
-
-// List.push(element);
-// List.push(ele);
-// List.push(ele);
-
-// Enqueue(e, List);
-
-// function Enqueue(node, arr) {
-//     if (arr.length < 1) {
-//         arr.push(node);
-//     // } else {
-//     //     var counter = 0;
-//     //     var there = 0
-//     //     while (counter < arr.length) {
-//     //         var there = (arr[counter].value == node.value) ? there++ : there;
-//     //         if (arr[counter].value < node.value) {
-//     //             //case 1 arr.next not null
-//     //             if (there <= 1)
-//     //                 arr.push(node);
-//     //             arr[arr.length - 1].previous = counter;
-//     //             arr[arr.length - 1].next = arr[counter].next;
-//     //             arr[counter].next = arr.length - 1;
-//     //             //case 2 arr.next null
-//     //         }
-//     //         // } else {
-//     //         //     var prv = arr[counter];
-//     //         //     node.previous =
-//     //         //         arr[counter] = node;
-//     //         // }
-//     //         counter++;
-//     //     }
-//     }
-// }
-
 function LinkedList() {
     return {
         head: null,
         tail: null,
         length: 0,
-        enqueue: function () { },
-        dequeue: function () { },
-        insert: function (value) {
+        enqueue: function (value) {
             var new_node = new Node(value);
             if (this.length < 1) {
                 this.head = new_node;
@@ -84,35 +44,81 @@ function LinkedList() {
                 this.tail = new_node;
             }
             this.length++;
-            return this;
         },
-        remove: function () { },
-        pop: function () {
+        dequeue: function () {
+            if (this.head == null) throw "List is empty.";
+            var old_head = this.head;
             if (this.length == 1) {
-                var n = head;
                 this.head = null;
                 this.tail = null;
-                return n;
+            } else {
+                this.head = old_head.next;
+                this.head.previous = null;
+                old_head.next = null;
             }
-            if (this.head == null) {
-                throw "List is empty."
-            }
-            var n = this.tail;
-            this.tail = n.previous;
-            this.tail.next = null;
-            this.length--
-            n.previous = null;
-            return n;
+            this.length--;
+            return old_head;
         },
-        displayInfo: function () { }
+        get: function (index) {
+            if (index <= 0 || index > this.length) throw "Can't get this element";
+            var counter = 1;
+            var current = this.head;
+            while (counter != index) {
+                current = current.next;
+                counter++;
+            }
+            return current;
+        },
+        insert: function () { },
+        remove: function (value) {
+            if (this.head == null) throw "List is empty.";
+            var counter = 0;
+            var node = this.head;
+            while (counter < this.length) {
+                if (value == node.value) {
+                    var target_node = node;
+                    console.log(target_node.previous)
+                    // target_node.previous = node.next;
+                    console.log(target_node.next);
+
+                    target_node.next = node.previous;
+                    // node.next = null;
+                    node.previous = null;
+                    return "found it";
+                }
+                node = node.next;
+                counter++;
+            }
+        },
+        pop: function () {
+            if (this.head == null) throw "List is empty.";
+            var popped_node = this.tail;
+            if (this.length == 1) {
+                this.head = null;
+                this.tail = null;
+            } else {
+                this.tail = popped_node.previous;
+                this.tail.next = null;
+                popped_node.previous = null;
+            }
+            this.length--;
+            return popped_node;
+        },
+        displayInfo: function () {
+            return this.head;
+        }
     }
 }
 var list = new LinkedList();
-list.insert(15);
-list.insert(12);
-list.insert(11);
-var x = list.insert(10);
-var r = list.pop();
+list.enqueue(15);//1
+list.enqueue(12);
+list.enqueue(11);//3
+list.enqueue(19);
+list.enqueue(20);//5
+// list.remove(19);
+var r = list.get(3);
+var rr = list.get(1);
+var rrr = list.get(5);
 console.log(r);
-console.log(x);
-
+console.log(rr);
+console.log(rrr);
