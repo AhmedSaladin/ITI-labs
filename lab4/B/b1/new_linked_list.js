7f56604472551e4889492bc1274488b5ff52bd9a
Ahmed Saladin <as.yuri74@gmail.com>
2021-04-28 00:18:52 +0200

    finishing task

--- lab4/B/b1/new_linked_list.js
+++ lab4/B/b1/new_linked_list.js
@@ -1,5 +1,9 @@
-// 7- Ensure that there is no duplication in your entered values.
+// 7- Ensure that there is no duplication in your entered values. --done
 // 8- You can add any property you need. --done
+function Node(val) {
+  this.value = val;
+}
+
 function LinkedList() {
   var list = [];
 
@@ -9,57 +13,58 @@ function LinkedList() {
   };
 }
 
-// 1- Enqueue a value as long as the value is in the sequence otherwise through --need to modifiy
-//    an exception(push an item at the end of the list with the passed value).
+// 1- Enqueue a value as long as the value is in the sequence otherwise through
+//    an exception(push an item at the end of the list with the passed value).  --done
 LinkedList.prototype.enqueue = function (item) {
+  var obj = this.create_new_node(item);
   var list = this.get_list();
-  if (this.search(item))
+  if (obj.value > list[0].value)
     throw "Push an item at the end of the list with the passed value";
-  list.unshift(item);
+  list.unshift(obj);
 };
 
 // 2- Insert an item in specific place as long as the value
-//    is missing from the sequence otherwise through an exception.--done
+//    is missing from the sequence otherwise through an exception.  --done
 LinkedList.prototype.insert = function (item, position) {
+  var obj = this.create_new_node(item);
   var list = this.get_list();
-  this.is_in_list(item);
-  list.splice(position - 1, 0, item);
+  list.splice(position - 1, 0, obj);
 };
 
-// 3- Pop a value (remove an item from the end of the list).--done
+// 3- Pop a value (remove an item from the end of the list).  --done
 LinkedList.prototype.pop = function () {
-  var list = this.get_list();
   this.is_empty();
+  var list = this.get_list();
   return list.pop();
 };
 
 // 4- Remove an item from specific place with the required value,
-//    if the value is not added return a message with “data not found”. --done
+//    if the value is not added return a message with “data not found”.  --done
 LinkedList.prototype.remove = function (value) {
-  var list = this.get_list();
   this.is_empty();
+  var list = this.get_list();
   var position = this.search(value);
   if (!position[0]) throw "Data not found";
   list.splice(position[1], 1);
 };
 
-// 5- Dequeue a value (remove an item from the beginning of the list). --done
+// 5- Dequeue a value (remove an item from the beginning of the list).  --done
 LinkedList.prototype.dequeue = function () {
-  var list = this.get_list();
   this.is_empty();
+  var list = this.get_list();
   return list.shift();
 };
 
 LinkedList.prototype.push = function (item) {
+  var obj = this.create_new_node(item);
   var list = this.get_list();
-  this.is_in_list(item);
-  list.push(item);
+  list.push(obj);
 };
 
 LinkedList.prototype.search = function (item) {
   var list = this.get_list();
   for (var i in list) {
-    if (item == list[i]) return [true, i];
+    if (item.value == list[i].value) return [true, i];
   }
   return [false];
 };
@@ -69,7 +74,21 @@ LinkedList.prototype.is_in_list = function (item) {
 };
 
 LinkedList.prototype.is_empty = function () {
-  if (this.get_list.length < 1) throw "List is empty";
+  if (this.get_list().length < 1) throw "List is empty";
+};
+
+LinkedList.prototype.sort = function () {
+  this.is_empty();
+  var list = this.get_list();
+  list.sort(function (a, b) {
+    return a.value - b.value;
+  });
+};
+
+LinkedList.prototype.create_new_node = function (item) {
+  var obj = new Node(item);
+  this.is_in_list(obj);
+  return obj;
 };
 
 var list = new LinkedList();
@@ -83,9 +102,10 @@ try {
   list.push(3);
   list.push(2);
   list.push(1);
-  list.push(1);
-  list.insert(30, 1);
-  // list.dequeue()
+  // list.push(1);
+  // list.insert(30, 1);
+  // list.dequeue();
+  // list.sort();
 } catch (err) {
   console.error(err);
 } finally {
