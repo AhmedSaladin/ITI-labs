463537f9cd6b7a09a743052177308b1c4906d8dc
Ahmed Saladin <as.yuri74@gmail.com>
2021-04-29 11:05:38 +0200

    adding lab5 tasks

--- lab4/ref/exc2/main.js
+++ lab4/ref/exc2/main.js
@@ -11,3 +11,21 @@ var defualt = myname();
 console.log(implicit());
 console.log(explicit);
 console.log(defualt);
+
+function Person(nm, ag, yr, id) {
+  this.name = nm;
+  this.age = ag;
+  this.bornYear = yr;
+  var ID = id;
+  this.getId = function () {
+    return this.ID;
+  };
+  this.setId = function (val) {
+    this.ID = val;
+  };
+}
+Person.hello = function () {
+  return "hello " + this.name;
+};
+var e = new Person("loki", 25, 1998, 1235);
+console.log(e);
