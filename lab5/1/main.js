8f49cc0acb15f069c0181b07f7550574e80f462e
Ahmed Saladin <as.yuri74@gmail.com>
2021-04-29 11:25:56 +0200

    adding files

---
+++ lab5/1/main.js
@@ -0,0 +1,14 @@
+/*
+linkedlist logic.
+-> Createyour own object that contains a list of numerical sequence, with the following details
+----> Your constructor takes 3 parameters to define start, end of list and step
+----> The list should be private and filled with private method
+----> You can create getter and setter for the list if needed
+----> Allow the user to apply the following functionality to hiscreated sequenceo
+--------> Append or prepend a new value (you have to ensure that you are pushing sequential
+          value otherwise through exception)
+ --------> Dequeue or pop a value,
+ --------> display all its content,
+ ----> all of the properties should be defined using accessor descriptor, prevent them from being deleted,iteratedor being modified.
+ ----> you can add any property you need.
+*/
