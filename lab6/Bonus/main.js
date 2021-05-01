1a799911f3b9c1d939f2f92fe088f092a4c4e8c4
Ahmed Saladin <as.yuri74@gmail.com>
2021-05-01 13:35:45 +0200

    finishing create_table and redesign html

--- lab6/Bonus/main.js
+++ lab6/Bonus/main.js
@@ -1,24 +1,30 @@
 /*
  --> Make ajax request to consume the service of “https://reqres.in/api/users”
-    ----> display email, first_name, and last_name fields in a table. 
+    ----> display email, first_name, and last_name fields in a table.  --done
 */
 
 function send_request() {
   var xhr = new XMLHttpRequest();
-  var respons;
   xhr.open("GET", "https://reqres.in/api/users");
   xhr.send("");
   xhr.onreadystatechange = function () {
-    if (this.status != 200) throw "Data not found 404";
-    respons = JSON.parse(xhr.responseText);
-    respons = respons.data;
+    if (this.readyState == 4) {
+      if (this.status != 200) throw "Data not found 404";
+      var res = this.responseText;
+      res = JSON.parse(res);
+      res = res.data;
+      for (var i in res) create_table(res[i]);
+    }
   };
-  return respons;
 }
 
-function create_table() {
-  var data = send_request();
-  var table = document.createElement("table");
-
-  console.log(table);
+function create_table(data) {
+  var table = document.getElementById("tbody");
+  var tr = document.createElement("tr");
+  tr.innerHTML = ` <td>${data.first_name}</td>
+    <td>${data.last_name}</td>
+    <td>${data.email}</td>`;
+  table.append(tr);
 }
+
+send_request();
