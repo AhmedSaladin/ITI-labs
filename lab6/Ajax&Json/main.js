58756c217b07b4061a6627ff0111864a2df80807
Ahmed Saladin <as.yuri74@gmail.com>
2021-05-03 16:47:30 +0200

    delete old function and apply new option func

--- lab6/Ajax&Json/main.js
+++ lab6/Ajax&Json/main.js
@@ -3,8 +3,6 @@ find out the rockbands.json file to make a page with 2 dropdown lists,
 the 1stone is filled with the band and the 2ndone is filledwith the artist after selecting a band from its dropdown list.
 Once the user selects an artist of the band;open his link
 */
-var bands = document.getElementById("bands");
-var members = document.getElementById("members");
 function send_request() {
   var xhr = new XMLHttpRequest();
   xhr.open("GET", "./rockbands.json", false);
@@ -14,6 +12,7 @@ function send_request() {
 }
 
 function set_bands_option() {
+  var bands = document.getElementById("bands");
   var data = send_request();
   for (var i in data) {
     var opt = document.createElement("option");
@@ -23,32 +22,15 @@ function set_bands_option() {
   }
 }
 
-function get_selected_band() {
-  var index = bands.selectedIndex;
-  var band = bands[index].value;
-  var data = send_request();
-  var team = data[band];
-  set_members_option(team);
-}
-
 function set_members_option(data) {
-  console.log("setm " + members.length);
+  var members = document.getElementById("members");
   for (var i in data) {
     var opt = document.createElement("option");
     opt.textContent = data[i].name;
     opt.value = data[i].value;
-    opt.className = "mem";
     members.appendChild(opt);
   }
 }
-
-function get_selected_member() {
-  var index = members.selectedIndex;
-  var member = members[index].value;
-  console.log(typeof member);
-  location.assign(member);
-}
-set_bands_option();
 
 function get_selected_option(id) {
   var tag = document.getElementById(id);
@@ -59,3 +41,5 @@ function get_selected_option(id) {
   var team = data[value];
   set_members_option(team);
 }
+
+set_bands_option();
