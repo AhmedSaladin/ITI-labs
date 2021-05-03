68941e7650ae10cdd40fec5f0dfba71813cb3c3d
Ahmed Saladin <as.yuri74@gmail.com>
2021-05-03 16:33:07 +0200

    fix get selected

--- lab6/Ajax&Json/main.js
+++ lab6/Ajax&Json/main.js
@@ -1,8 +1,10 @@
 /*
 find out the rockbands.json file to make a page with 2 dropdown lists,
- the 1stone is filled with the band and the 2ndone is filledwith the artist after selecting a band from its dropdown list.
+the 1stone is filled with the band and the 2ndone is filledwith the artist after selecting a band from its dropdown list.
 Once the user selects an artist of the band;open his link
 */
+var bands = document.getElementById("bands");
+var members = document.getElementById("members");
 function send_request() {
   var xhr = new XMLHttpRequest();
   xhr.open("GET", "./rockbands.json", false);
@@ -13,24 +15,37 @@ function send_request() {
 
 function set_bands_option() {
   var data = send_request();
-  var tag = document.getElementById("bands");
-  console.log(data);
   for (var i in data) {
     var opt = document.createElement("option");
     opt.textContent = i;
     opt.value = i;
-    tag.appendChild(opt);
+    bands.appendChild(opt);
   }
 }
 
-// function set_members_option(data) {
-//   var tag = document.getElementById("members");
-//   var opt = document.createElement("option");
-//   for (var i in data) {
-//     opt.textContent = data[i].name;
-//     opt.value = data[i].value;
-//     tag.appendChild(opt);
-//   }
-// }
-send_request();
+function get_selected_band() {
+  var index = bands.selectedIndex;
+  var band = bands[index].value;
+  var data = send_request();
+  var team = data[band];
+  set_members_option(team);
+}
+
+function set_members_option(data) {
+  console.log("setm " + members.length);
+  for (var i in data) {
+    var opt = document.createElement("option");
+    opt.textContent = data[i].name;
+    opt.value = data[i].value;
+    opt.className = "mem";
+    members.appendChild(opt);
+  }
+}
+
+function get_selected_member() {
+  var index = members.selectedIndex;
+  var member = members[index].value;
+  console.log(typeof member);
+  location.assign(member);
+}
 set_bands_option();
