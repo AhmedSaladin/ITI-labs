fe3c111104217df83a9a78ecd922e1aa45571a2c
Ahmed Saladin <as.yuri74@gmail.com>
2021-05-04 13:43:41 +0200

    Remove old structure

--- lab4/C/c1/main.js
+++
@@ -1,8 +0,0 @@
-function only_two(a, b) {
-    if (arguments.length > 2)
-        throw "This function accept only two parameter!";
-    if (arguments.length <= 0)
-        throw "This function accept two parameter!";
-    return [a, b];
-}
-// console based
--- lab4/C/c3/main.js
+++
@@ -1,7 +0,0 @@
-second_rev = new Function([], "console.log([].reverse.apply(arguments))");
-
-function rev() {
-    var res = [].reverse.bind(arguments);
-    return res();
-}
-// console based
