d432d2b9e90277e27d4dcc2a7141eb053cd3dd5e
Ahmed Saladin <as.yuri74@gmail.com>
2021-05-07 03:41:42 +0200

    create road and style it

--- JQ/lab2/car_game/src/main.js
+++ JQ/lab2/car_game/src/main.js
@@ -1,18 +1,33 @@
+var winner = "";
 $("#start").click(function () {
   $("#p1").animate(
     {
       top: "-=750px",
     },
-    ran()
+    ran(),
+    function () {
+      $("#p2").stop();
+      winner = "Player 1";
+      win();
+    }
   );
   $("#p2").animate(
     {
       top: "-=750px",
     },
-    ran()
+    ran(),
+    function () {
+      $("#p1").stop();
+      winner = "Player 2";
+      win();
+    }
   );
 });
 
 function ran() {
   return Math.floor(Math.random() * 5000);
+}
+function win() {
+  console.log(winner);
+  $("#win").val(winner);
 }
