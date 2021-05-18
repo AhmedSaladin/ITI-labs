8f4cded7b85e55724e6aa414f0b28c8e3c6ca636
Ahmed Saladin <as.yuri74@gmail.com>
2021-05-18 13:16:54 +0200

    finishing task2

--- ES6/lab1/main.js
+++ ES6/lab1/main.js
@@ -1,7 +1,22 @@
 //  1- destructuring
 let x = 5,
   y = 10;
+console.log(`Before swap x: ${x}, y: ${y}`);
+
 [x, y] = [y, x];
-console.log(x, y);
- 
-//
+
+console.log(`After swap x: ${x}, y: ${y}`);
+
+// 2- Using rest parameter and spread operator
+let a = [1, 7, 5, 8, 9, 2, 0, -1];
+
+let max = (array) => {
+  return Math.max(...array);
+};
+
+let min = (array) => {
+  return Math.min(...array);
+};
+
+console.log(`The minimum:${min(a)}`);
+console.log(`The maximum:${max(a)}`);
